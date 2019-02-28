00723c92104b2c79a9b4448cd479ec23340068f8
udowa_m <udowa_m@L-HOBB-0113-04>
2019-02-28 17:00:02 -0500

    save

--- GraphLib/GraphLib/SymbolGraph.cs
+++ GraphLib/GraphLib/SymbolGraph.cs
@@ -5,30 +5,89 @@ using System.IO;
 
 namespace GraphLib
 {
-    //public class SymbolGraph : ISymbolGraph, IGraph
-    //{
-    //    public SymbolGraph(string filename, char delimiter)
-    //    {
-    //        Graph = new BasicGraph();
-    //        StreamReader reader = new StreamReader(filename);
-    //        string nextLine;
-    //        while (!reader.EndOfStream)
-    //        {
-    //            nextLine = reader.ReadLine();
-    //            Delimiter[0] = delimiter;
-    //            string[] keys =
-    //                nextLine.Split(Delimiter, StringSplitOptions.RemoveEmptyEntries);
+    public class SymbolGraph : ISymbolGraph, IGraph
+    {
+        public SymbolGraph()
+        {
 
-    //            string source = keys[0];
-    //            for (int i = 1; i < keys.Length; i++)
-    //            {
-    //                AddEdge(source, keys[i]);
-    //            }
-    //        }
-    //    }
+        }
+        public SymbolGraph(string filename, char delimiter)
+        {
+            Graph = new BasicGraph();
+            StreamReader reader = new StreamReader(filename);
+            string nextLine;
+            while (!reader.EndOfStream)
+            {
+                nextLine = reader.ReadLine();
+                Delimiter[0] = delimiter;
+                string[] keys =
+                    nextLine.Split(Delimiter, StringSplitOptions.RemoveEmptyEntries);
 
-    //    public IGraph Graph { get; }
-    //    char[] Delimiter = new char[1];
-    //}
+                string source = keys[0];
+                for (int i = 1; i < keys.Length; i++)
+                {
+                    AddEdge(source, keys[i]);
+                }
+            }
+        }
+
+        public IGraph Graph { get; }
+
+        Dictionary<string, int> KeyToVertexId = new Dictionary<string, int>();
+        Dictionary<int, string> VertexIdToKey = new Dictionary<int, string>();
+
+        public int NVertices
+        {
+            get
+            {
+                return Graph.NVertices;
+            }
+        }
+
+        public int NEdges
+        {
+            get
+            {
+                return Graph.NEdges;
+            }
+        }
+
+        char[] Delimiter = new char[1];
+
+        public void AddNode(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddEdge(string firstVertex, string secondVertex)
+        {
+            Graph.AddEdge(firstVertex, secondVertex);
+        }
+
+        public bool Contains(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string NameOf(int vertex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddEdge(int firstVertex, int secondVertex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<int> GetAdjacentVertices(int vertexId)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
 
--- GraphLib/GraphTests/UnitTest1.cs
+++ GraphLib/GraphTests/UnitTest1.cs
@@ -8,39 +8,39 @@ namespace GraphTests
     [TestClass]
     public class GraphTests
     {
-        //    [TestMethod]
-        //    public void createGraph()
-        //    {
-        //        BasicGraph myGraph = new BasicGraph(2);
-        //        myGraph.AddEdge(0, 1);
-        //        foreach (var vertex in myGraph.GetAdjacentVertices(1))
-        //        {
-        //            Assert.AreEqual(0, vertex);
-        //        }
-        //    }
+        [TestMethod]
+        public void createGraph()
+        {
+            BasicGraph myGraph = new BasicGraph(2);
+            myGraph.AddEdge(0, 1);
+            foreach (var vertex in myGraph.GetAdjacentVertices(1))
+            {
+                Assert.AreEqual(0, vertex);
+            }
+        }
 
-        //    [TestMethod]
-        //    public void createEmptyGraph()
-        //    {
-        //        BasicGraph myGraph = new BasicGraph();
-        //        myGraph.AddEdge(0, 1);
-        //        foreach (var vertex in myGraph.GetAdjacentVertices(1))
-        //        {
-        //            Assert.AreEqual(0, vertex);
-        //        }
-        //    }
+        [TestMethod]
+        public void createEmptyGraph()
+        {
+            BasicGraph myGraph = new BasicGraph();
+            myGraph.AddEdge(0, 1);
+            foreach (var vertex in myGraph.GetAdjacentVertices(1))
+            {
+                Assert.AreEqual(0, vertex);
+            }
+        }
 
-        //    [TestMethod]
-        //    public void createSymbolGraph()
-        //    {
-        //        SymbolGraph myGraph = new SymbolGraph();
-        //        myGraph.AddEdge("a", "b");
-        //        foreach (var vertex in
-        //            myGraph.GetAdjacentVertices(myGraph.KeyToVertexId["a"]))
-        //        {
-        //            Assert.AreEqual("b", myGraph.VertexIdToKey[vertex]);
-        //        }
-        //    }
+        [TestMethod]
+        public void createSymbolGraph()
+        {
+            SymbolGraph myGraph = new SymbolGraph();
+            myGraph.AddEdge("a", "b");
+            foreach (var vertex in
+                myGraph.GetAdjacentVertices(myGraph.KeyToVertexId["a"]))
+            {
+                Assert.AreEqual("b", myGraph.VertexIdToKey[vertex]);
+            }
+        }
 
         //    Move oneEntry.txt to the bin/Debug of the test project.
         //    [TestMethod]
@@ -120,43 +120,43 @@ namespace GraphTests
         //        }
         //    }
 
-    // movies.txt is available here: https://algs4.cs.princeton.edu/41graph/movies.txt
-    //    [TestMethod];
-    //    public void readwholeMovieFile()
-    //    {
-    //        SymbolGraph myGraph = new SymbolGraph("movies.txt", '/');
-    //        int counter = 0;
-    //        foreach (int vertex in
-    //            myGraph.GetAdjacentVertices(myGraph.KeyToVertexId["'Breaker' Morant (1980)"]))
-    //        {
-    //            if (counter == 0)
-    //            {
-    //                Assert.AreEqual("Brown, Bryan (I)",
-    //                    myGraph.VertexIdToKey[vertex]);
-    //            }
-    //            ++counter;
-    //        }
-    //        Assert.AreEqual(42, counter);
-    //    }
+        // movies.txt is available here: https://algs4.cs.princeton.edu/41graph/movies.txt
+        //    [TestMethod];
+        //    public void readwholeMovieFile()
+        //    {
+        //        SymbolGraph myGraph = new SymbolGraph("movies.txt", '/');
+        //        int counter = 0;
+        //        foreach (int vertex in
+        //            myGraph.GetAdjacentVertices(myGraph.KeyToVertexId["'Breaker' Morant (1980)"]))
+        //        {
+        //            if (counter == 0)
+        //            {
+        //                Assert.AreEqual("Brown, Bryan (I)",
+        //                    myGraph.VertexIdToKey[vertex]);
+        //            }
+        //            ++counter;
+        //        }
+        //        Assert.AreEqual(42, counter);
+        //    }
 
-    //    [TestMethod]
-    //    public void findMoviePath()
-    //    {
-    //        SymbolGraph myGraph = new SymbolGraph("movies.txt", '/');
-    //        int sourceVertex = myGraph.KeyToVertexId["Animal House (1978)"];
-    //        BreadthFirstPaths bfPaths = new BreadthFirstPaths(myGraph, sourceVertex);
-    //        int i = 0;
-    //        List<string> result = new List<string>();
-    //        result.Add("Animal House (1978)");
-    //        result.Add("Vernon, John (I)");
-    //        result.Add("Topaz (1969)");
-    //        result.Add("Charles, Lewis");
-    //        result.Add("To Catch a Thief (1955)");
-    //        int destination = myGraph.KeyToVertexId["To Catch a Thief (1955)"];
-    //        foreach (int inPath in bfPaths.PathTo(destination))
-    //        {
-    //            Assert.AreEqual(result[i++], myGraph.VertexIdToKey[inPath]);
-    //        }
-    //    }
+        //    [TestMethod]
+        //    public void findMoviePath()
+        //    {
+        //        SymbolGraph myGraph = new SymbolGraph("movies.txt", '/');
+        //        int sourceVertex = myGraph.KeyToVertexId["Animal House (1978)"];
+        //        BreadthFirstPaths bfPaths = new BreadthFirstPaths(myGraph, sourceVertex);
+        //        int i = 0;
+        //        List<string> result = new List<string>();
+        //        result.Add("Animal House (1978)");
+        //        result.Add("Vernon, John (I)");
+        //        result.Add("Topaz (1969)");
+        //        result.Add("Charles, Lewis");
+        //        result.Add("To Catch a Thief (1955)");
+        //        int destination = myGraph.KeyToVertexId["To Catch a Thief (1955)"];
+        //        foreach (int inPath in bfPaths.PathTo(destination))
+        //        {
+        //            Assert.AreEqual(result[i++], myGraph.VertexIdToKey[inPath]);
+        //        }
+        //    }
     }
 }
